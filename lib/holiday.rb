b1c3f024b97b00b113da7ca55e096960af28256a
Renée Cruz <reneebasscruz@gmail.com>
2019-07-28 23:31:54 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,8 +76,13 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   
-  holiday_hash.each 
-
+  holiday_hash.each do |key, value|
+    key.to_s
+    value.to_s
+    puts "#{key}:" 
+    puts "#{value}"
+    binding.pry
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
